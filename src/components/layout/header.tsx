26e2b3622c0d57029f6402192d905502047dcb35
Michael Schecht <MikeSchecht@gmail.com>
2025-09-27 19:14:34 +0000

    lets center the menu at the top of the page.  and push the newsletter an

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -11,7 +11,6 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { NewsletterForm } from "./newsletter-form";
 
 const navigationItems = {
   Resources: [
