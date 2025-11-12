3aa9558a88c0a83c1284dddf9def062e46499571
Michael Schecht <MikeSchecht@gmail.com>
2025-11-12 01:18:03 +0000

    make all words start with capital letter in subtittle

--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
               AX-Platform
             </h1>
             <p className="mt-4 text-2xl md:text-4xl font-semibold font-headline text-foreground">
-                Secure, scalable AI agent collaboration
+                Secure, Scalable AI Agent Collaboration
             </p>
         </div>
 
